7fc70d4be7213d9b0235aa67ff877f83ba89c48d
Zhixin Chen <zc2575@columbia.edu>
2021-04-05 03:34:54 +0000

    Add views

---
+++ views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from .models import Squirrel
+from .forms import SquirrelForm
